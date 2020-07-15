d668b58f837e6d517ecc99cfc3cdef2f5201826f
unrealspecter <tomi_emmen@hotmail.com>
2020-07-15 23:41:38 +0200

    added menu items

--- resources/js/Core/Navigation.vue
+++ resources/js/Core/Navigation.vue
@@ -1,7 +1,9 @@
 <template>
     <nav class="z-50 h-100 w-screen fixed top-0 left-0 shadow-lg bg-white flex justify-between px-50 items-center montserrat-bold uppercase">
         <a href="/">Level Up!</a>
-        <a href="/modules">Dashboard</a>
+        <a href="/dashboard">Profile</a>
+        <a href="/modules">Level Overzicht</a>
+        <a href="/courses">Course Overzicht</a>
         <a href="/logout" onclick="event.preventDefault(); document.getElementById('frm-logout').submit();">Logout</a>
         <form id="frm-logout" action="/logout" method="POST" class="hidden">
             <input type="hidden" name="_token" :value="csrf">
